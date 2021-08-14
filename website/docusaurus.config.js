8654fab099f6408a4e05390ac291c73cf474cfc0
bzp2010 <bzp2010@apache.org>
2021-08-14 13:30:09 +0800

    add apisix and dashboard auto releases

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -230,6 +230,14 @@ module.exports = {
       },
     ],
     [
+      "@docusaurus/plugin-content-blog",
+      {
+        id: "releases",
+        routeBasePath: "releases",
+        path: "releases",
+      },
+    ],
+    [
       "@docusaurus/plugin-content-docs",
       {
         id: "docs-apisix",
